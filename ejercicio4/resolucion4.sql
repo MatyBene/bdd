fedc73d403f2c43bb45e5c541f973d6691cdfa5b
MatyBene <benedettimaty@gmail.com>
2024-08-05 15:51:51 -0300

    comienza resolucion ejercicio 4

---
+++ ejercicio4/resolucion4.sql
@@ -0,0 +1,48 @@
+CREATE TABLE torneo;
+USE torneo;
+
+CREATE TABLE Participantes(
+    dni_participante ,
+    apellido ,
+    nombre ,
+    direccion ,
+    edad ,
+    telefono ,
+    sexo
+);
+
+CREATE TABLE Categorias(
+    id_categoria ,
+    nombre ,
+    edad_min ,
+    edad_max ,
+    sexo
+);
+
+CREATE TABLE Inscripciones(
+    num_inscripcion ,
+    fecha_inscripcion ,
+    abono_inscripcion
+);
+
+INSERT INTO Participantes() VALUES
+
+INSERT INTO Categorias() VALUES
+
+INSERT INTO Inscripciones() VALUES
+
+/* CONSULTAS SQL */
+
+/* 1 */
+
+/* 2 */
+
+/* 3 */
+
+/* 4 */
+
+/* 5 */
+
+/* 6 */
+
+/* 7 */
